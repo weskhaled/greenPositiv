84f0e732de4acbbda1a50f1ebc03ac54bca03ccc
weskhaled <weskhaled@gmail.com>
2022-03-14 23:39:34 +0100

    feat: update and delete experience

--- src/api/modules/freelancer.ts
+++ src/api/modules/freelancer.ts
@@ -4,7 +4,7 @@ const freelancerApi = {
   updateProfile: async(params: object) => service.patch('/freelancer', params),
   addExperience: async(params: object) => service.post('/freelancer/add-experience', params),
   updateExperience: async(id: string, params: object) => service.patch(`/freelancer/update-experience/${id}`, params),
-  deleteExperience: async(id: string) => service.delete(`/freelancer/delete-experience/${id}`),
+  deleteExperience: async(id: string) => service.patch(`/freelancer/delete-experience/${id}`),
 }
 
 export default freelancerApi
