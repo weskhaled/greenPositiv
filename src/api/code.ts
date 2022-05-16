10a3e998a76dc80b129d58ad5fe6a3943635bda9
youssef <mohamedyoussef.jemai@gmail.com>
2022-05-16 17:53:16 +0200

    message en fr

--- src/api/code.ts
+++ src/api/code.ts
@@ -10,7 +10,7 @@ declare interface codeMessageMapTypes {
 
 const codeMessageMap: codeMessageMapTypes = {
   400: '[400]:wrong request parameter',
-  401: '[401]:Account not logged in',
+  401: '[401]:Vous avez été déconnecté',
   403: '[403]:access denied',
   404: '[404]:request path error',
   405: '[405]:request method error',
